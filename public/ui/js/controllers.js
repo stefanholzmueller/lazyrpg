4df5dfbd568e23f180541e23625d54a580d17d67
Stefan Holzmüller <stefanholzmueller@github>
2013-06-19 07:33:34 +0200

    client-side login validation

--- public/ui/js/controllers.js
+++ public/ui/js/controllers.js
@@ -1,5 +1,37 @@
 'use strict';
 
+lazyRPG.controller("LoginController", function($scope, $window) {
+	$scope.login = function() {
+		var username = $scope.username;
+		var errors = validateUsername(username);
+		
+		if (errors.length === 0) {
+			redirectToPlayPage(username);
+		} else {
+			$scope.errors = errors;
+		}
+	}
+
+	function validateUsername(username) {
+		var errors = [];
+		
+		if (username) {
+			if (username.length < 3 && username.length > 30) {
+				errors.push("Usernames must have between 3 and 30 characters");
+			}
+		} else {
+			errors.push("Username must not be empty");
+		}
+		
+		return errors;
+	}
+
+	function redirectToPlayPage(username) {
+		$window.location.href = "http://" + document.location.host + "/play/"
+				+ encodeURIComponent(username);
+	}
+});
+
 lazyRPG.controller("PlayerController", function($scope, $window, wsFactory) {
 	var escapedUsername = $window.location.pathname.split("/")[2]
 	var username = decodeURIComponent(escapedUsername);
