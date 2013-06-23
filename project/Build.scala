4bda05d2abef28fdd61d184ba1a8937691ffb60f
Stefan Holzmüller <stefanholzmueller@github>
2013-06-23 21:17:28 +0200

    preparations for automatic builds

--- project/Build.scala
+++ project/Build.scala
@@ -1,23 +1,23 @@
 import sbt._
 import Keys._
 import play.Project._
+import cloudbees.Plugin._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "lazyrpg"
-  val appVersion      = "1.0-SNAPSHOT"
+	val appName = "lazyrpg"
+	val appVersion = "1.0-SNAPSHOT"
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm,
-    "com.typesafe.akka" % "akka-testkit_2.10" % "2.1.1" % "test",
-    "org.scalatest" % "scalatest_2.10" % "2.0.M5b" % "test"
-  )
+	val appDependencies = Seq(
+		// Add your project dependencies here,
+		jdbc,
+		anorm,
+		"com.typesafe.akka" % "akka-testkit_2.10" % "2.1.1" % "test",
+		"org.scalatest" % "scalatest_2.10" % "2.0.M5b" % "test")
 
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+	val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA)
+		.settings(cloudBeesSettings: _*)
+		.settings(
+			CloudBees.applicationId := Some("m1"))
 
 }
