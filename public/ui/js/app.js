89a7e43d675332ee3850c24defc0964d6e3f4023
Stefan Holzmüller <stefanholzmueller@github>
2013-06-24 19:48:16 +0200

    set html5Mode to avoid hashbangs

--- public/ui/js/app.js
+++ public/ui/js/app.js
@@ -1,3 +1,6 @@
 'use strict';
 
-var lazyRPG = angular.module("lazyrpg", [ "luegg.directives" ]);
+var lazyRPG = angular.module("lazyrpg", [ "luegg.directives" ], function(
+		$locationProvider) {
+	$locationProvider.html5Mode(true)
+});
